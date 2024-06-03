f999cbf418c1d45d0ef5db483241d084a3b78d77
tunk <nguyenkhactu1996@gmail.com>
2024-06-03 22:14:42 +0700

    Initial commit

---
+++ src/main/java/vn/edu/vti/vmall/vmall_discovery_service/VmallDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package vn.edu.vti.vmall.vmall_discovery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class VmallDiscoveryServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VmallDiscoveryServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/vn/edu/vti/vmall/vmall_discovery_service/VmallDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.vti.vmall.vmall_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmallDiscoveryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
