dd06d397e9bd23eb5002f82543bf5378d749d470
tunk <nguyenkhactu1996@gmail.com>
2024-06-14 09:21:42 +0700

    Change package and dir name

--- src/main/java/vn/edu/vti/vmall/discovery/service/VmallDiscoveryServiceApplication.java
+++ src/main/java/vn/edu/vti/vmall/discovery/service/VmallDiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package vn.edu.vti.vmall.vmall_discovery_service;
+package vn.edu.vti.vmall.discovery.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/vn/edu/vti/vmall/discovery/service/VmallDiscoveryServiceApplicationTests.java
+++ src/test/java/vn/edu/vti/vmall/discovery/service/VmallDiscoveryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package vn.edu.vti.vmall.vmall_discovery_service;
+package vn.edu.vti.vmall.discovery.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
