550be22863cb351c85eea2badd9a7cc00685d8b3
tunk <nguyenkhactu1996@gmail.com>
2024-06-14 09:51:08 +0700

    Change package  name

--- src/main/java/vn/edu/vti/vmall/service/discovery/VmallDiscoveryServiceApplication.java
+++ src/main/java/vn/edu/vti/vmall/service/discovery/VmallDiscoveryServiceApplication.java
@@ -1,4 +1,4 @@
-package vn.edu.vti.vmall.discovery.service;
+package vn.edu.vti.vmall.service.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/vn/edu/vti/vmall/service/discovery/VmallDiscoveryServiceApplicationTests.java
+++ src/test/java/vn/edu/vti/vmall/service/discovery/VmallDiscoveryServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package vn.edu.vti.vmall.discovery.service;
+package vn.edu.vti.vmall.service.discovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
